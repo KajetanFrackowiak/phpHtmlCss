80534bb00a1fa3ce62989bf8cdd77a8e8830a6f2
KajetanFrackowiak <115082135+KajetanFrackowiak@users.noreply.github.com>
2023-04-11 09:16:56 +0200

    Unfinished calculation of the average

---
+++ Kurs PHP Kanał o wszystkim/Course12/process12.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Average grades</title>
+    <link rel="stylesheet" href="style12.php.css"
+</head>
+<body>
+<?php
+if (isset($_GET['num']) and is_numeric($_GET['num'])) {
+
+} else {
+    echo '<br>Not ratings given';
+}
+?>
+</body>
+</html>
