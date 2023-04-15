304dcfae37eab866c9b0eea9f3d153297ffe39db
KajetanFrackowiak <115082135+KajetanFrackowiak@users.noreply.github.com>
2023-04-15 09:16:58 +0200

    Sessions not finished else

--- Kurs PHP Kanał o wszystkim/Course16/process16.php
+++ Kurs PHP Kanał o wszystkim/Course16/process16.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+
+    if (isset($_POST['login']) and isset($_POST['pass'])) {
+        $_SESSION['user'] = $_POST['login'];
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +15,13 @@
 </head>
 <body>
 <?php
-
+    if (isset($_SESSION['user'])) {
+        echo '<b>Logged in</b><br><br>';
+        echo 'LOGIN: ';
+        echo $_SESSION['user'];
+    } else {
+        echo 'Not logged in';
+    }
 ?>
 </body>
 </html>
