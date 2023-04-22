75cefe86d1ccd03ce9854b8a1b923d2e62879d0a
Kajetan <kajtek.gdynia@gmail.com>
2023-04-22 12:23:53 +0200

    .

--- Kurs PHP pasja informatyki/episode 1/order2.php
+++ Kurs PHP pasja informatyki/episode 1/order2.php
@@ -13,7 +13,7 @@
     $suma = 0.99*$paczkow + 1.29*$grzebieni;
 //zamiast w kółko pisać echo, możemy napisać echo<<<END i będzie to w każdym wierszu na początku automatycznie pisało echo aż nie napotka END;
 //żeby to działało ten napis nie może być poprzedzony żadnym znakiem czy tabem czy nawet spacją i tak samo END;
-echo<<<END
+echo <<<END
     <h2>Podsumowanie zamówienia</h2>
     <table border="1" cellpadding="10" cellspacing="0"> <!--table border="1" to tabela grubości 1, cell(komórka)padding(wypełnienie), cellpadding="10" zrobi, że będzie w każdej komórce wokół tekstu 10px wolnego miejsc, cellspasing (odsęp pomiędzy komórkami)-->
     <!-- <td></td> = konórka tabeli, <tr></tr> = cały wiersz. tr zamyk nam wiersz i powoduje przejście do nowej linii -->
---
+++ Kurs PHP pasja informatyki/episode 2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Cmpatible" content="IE=edge,chrome=1">
+<title>Osadnicy</title>
+<head>
+
+</head>
+<body>
+Tylko martwi urzeli koniec wojny - Platon<br><br>
+
+<form action="zaloguj.php" method="post">
+    Login: <br> <input type="text" name="login"> <br>
+    Hasło: <br> <input type="password" name="haslo"> <br><br>
+    <input type="submit" value="Zaloguj się">
+</form>
+</body>
+</html>
