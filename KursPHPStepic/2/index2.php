9ac53be1183ce191e687364687df085002815e3e
KajetanFrackowiak <115082135+KajetanFrackowiak@users.noreply.github.com>
2023-04-30 09:41:12 +0200

    .

---
+++ KursPHPStepic/2/index2.php
@@ -0,0 +1,22 @@
+<!--<<<END-->
+<!--    END;-->
+
+<!--<<<EOF-->
+<!--    EOF;-->
+                        <!-- strcmp -->
+<!--rawurldencode-->   <!--soundex, metaphone-->
+<!--rawurldecode-->    <!-- similar_text -->
+                        <!-- levenshtein -->
+<!--urldecode-->       <!-- <<< title text title;    -->
+<!--urlencode-->        <!-- substr_count -->
+                        <!-- substr_replace($gretting, "and bye, bye.", 13, strlen($gretting) - 1)-->
+                        <!-- strrev  , a function to write the reverse a sentence-->
+                        <!-- str_repeat -->
+                        <!-- $str = str_pad("Fred Flintstone", 69);-->
+
+
+<?php
+$str = str_pad("Fred Flintstone", 69); //
+echo "{$str}:35:Wilma";
+?>
+
