83d111075781cc7edfa0182eac188982a028b56a
Kajetan <kajtek.gdynia@gmail.com>
2023-05-04 09:40:00 +0200

    ;

--- KursPHPStepic/2/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$input = readline();
-$basename = basename($input);
-//$position = strpos($basename, '.');
-//$name = substr($basename, 0, $position);
-print "$basename";
-?>
---
+++ KursPHPStepic/2/table.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <title>Title</title>
+    <meta charset="UTF-8">
+</head>
+<div>
+    <table>
+        <tr>
+            <th>Produkt</th>
+            <th>Opis</th>
+            <th>Cena</th>
+            <th>Zdjęcie</th>
+        </tr>
+
+        <tr>
+            <td><a href="https://www.mediaexpert.pl/telewizory-i-rtv/telewizory/telewizor-samsung-qe55s95b-55-oled-4k-120hz-tizen-tv-dolby-atmos-hdmi-2-1-dvb-t2-hevc-h-265">
+                    Telewizor</a></td>
+            <td>Duży telewizor LED o rozmiarze 55 cali.</td>
+            <td>7969 zł</td>
+            <td width="30%">
+                <a href="https://www.mediaexpert.pl/telewizory-i-rtv/telewizory/telewizor-samsung-qe55s95b-55-oled-4k-120hz-tizen-tv-dolby-atmos-hdmi-2-1-dvb-t2-hevc-h-265">
+                    <img src="https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/38/3835862/Telewizor-SAMSUNG-QE55S95B-skos-lewy.jpg"
+                         width="15%" alt="Telewizor">
+                </a></td>
+
+        </tr>
+
+        <tr>
+            <td><a href="https://www.mediaexpert.pl/komputery-i-tablety/laptopy-i-ultrabooki/laptopy/laptop-dell-inspiron-3525-6518-15-6-ips-r5-5625u-8gb-ram-512gb-ssd-windows-11-home">
+                    Laptop</a></td>
+            <td>Laptop z procesorem Intel Core i7 i dyskiem SSD.</td>
+            <td>3199 zł</td>
+            <td width="30%">
+                <a href="https://www.mediaexpert.pl/komputery-i-tablety/laptopy-i-ultrabooki/laptopy/laptop-dell-inspiron-3525-6518-15-6-ips-r5-5625u-8gb-ram-512gb-ssd-windows-11-home">
+                    <img src="https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/36/3652913/Laptop-DELL-Inspiron-3525-6518-1.jpg"
+                         width="15%" alt="Laptop">
+                </a></td>
+
+
+
+
+        </tr>
+
+        <tr>
+            <td><a href="https://www.mediaexpert.pl/foto-i-kamery/aparaty-fotograficzne/lustrzanki-cyfrowe/aparat-canon-eos-250d-bk-18-55-s-cp-eu26">
+                    Kamera</a></td>
+            <td>Kamera z opcją nagrywania w jakości 4K.</td>
+            <td>3699 zł</td>
+            <td width="30%"><a href="https://www.mediaexpert.pl/foto-i-kamery/aparaty-fotograficzne/lustrzanki-cyfrowe/aparat-canon-eos-250d-bk-18-55-s-cp-eu26">
+                    <img src="https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/21/2112461/Aparat-CANON-EOS-250D-Obiektyw-Canon-EF-S-18-55-mm-f-4-5-6-IS-STM-skos.jpg"
+                         width="15%" alt="Kamera">
+
+                </a></td>
+        </tr>
+
+        <tr>
+            <td><a href="https://www.mediaexpert.pl/smartfony-i-zegarki/smartfony/smartfon-xiaomi-redmi-note-12-4-128gb-onyx-gray">
+                    Smartfon</a></td>
+            <td>Smartfon z dużym wyświetlaczem i podwójnym aparatem.</td>
+            <td>1099 zł</td>
+            <td width="30%">
+                <a href="https://www.mediaexpert.pl/smartfony-i-zegarki/smartfony/smartfon-xiaomi-redmi-note-12-4-128gb-onyx-gray">
+                    <img src="https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/52/5256298/Smartfon-XIAOMI-Redmi-Note-12-4-128GB-6.67-120Hz-Czarny-tyl-front.jpg"
+                         width="15%" alt="Smartfon">
+                </a></td>
+
+        </tr>
+    </table>
+</div>
+
+<body>
+
+</body>
+</html>
