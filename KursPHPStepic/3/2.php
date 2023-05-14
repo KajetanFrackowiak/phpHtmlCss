e0e7f53b29eaf82b540e6754de8b10ee5cccfdc0
KajetanFrackowiak <115082135+KajetanFrackowiak@users.noreply.github.com>
2023-05-14 09:27:23 +0200

    objectivity part two

--- KursPHPStepic/3/2.php
+++ KursPHPStepic/3/2.php
@@ -107,7 +107,7 @@ echo $anonymous->getName();*/
 */?>
 
 <?php
-class Person {
+/*class Person {
     private $name = '';
 
     public function getName(): string {
@@ -130,5 +130,116 @@ class Employee extends Person {
     }
 }
 var_dump(get_class_methods("Employee")); // od parametrow
+var_dump(get_class_vars(Employee::class));
 var_dump(get_class_vars("Employee")); // od pol
+*/?>
+
+<?php
+/*class A {
+    public $name = '';
+    public $secondName = '';
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+    public function getName(): string {
+        return $this->name;
+    }
+
+}
+class B extends A {}
+$obj = new B;
+echo  get_parent_class($obj) . "\n";
+echo get_parent_class(B::class) . "\n";
+
+$var = new A();
+$var->setName('Kajetan');
+$var->secondName = "Frackowiak";
+var_dump(get_class_vars("A"));
+var_dump(get_object_vars($var));
+*/?>
+
+<?php
+/*    class Log {
+        private $fh;
+        function __construct($filename) {
+            $this->filename = $filename;
+            $this->open();
+        }
+        function open() {
+            $this->fh = fopen($this->filename, 'a') or die("Can't open {$this->filename}");
+        }
+        function write($note) {
+            fwrite($this->fh, "{$note}\n");
+        }
+        function read() {
+            return join('', file($this->filename));
+        }
+        function __wakeup(): void
+        {
+            $this->filename = 'persistent_log';   // TODO: Implement __wakeup() method.
+            $this->open();
+        }
+        function __sleep(): array
+        {
+            fclose($this->fh);   // TODO: Implement __sleep() method.
+            return ["filename" => $this->filename];
+        }
+    }
+*/?>
+<?php
+interface Movable {
+    public function moveUp():void;
+    public function moveDown():void;
+    public function moveLeft():void;
+    public function moveRight():void;
+}
+
+class MovablePoint implements Movable {
+    private $x;
+    private $y;
+    private $xSpeed;
+    private $ySpeed;
+    public function __construct($x, $y, $xSpeed, $ySpeed)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->xSpeed = $xSpeed;
+        $this->ySpeed = $ySpeed;
+    }
+    public function __toString(): string
+    {
+        return "x: " . $this->x . ", y: " . $this->y . "\n";// TODO: Implement __toString() method.
+    }
+
+    public function moveUp(): void
+    {
+         echo $this->y  += $this->ySpeed; // TODO: Implement moveUp() method.
+        echo "\tMove up\n";
+    }
+    public function moveDown(): void
+    {
+        echo $this->y -= $this->ySpeed;// TODO: Implement moveDown() method.
+        echo "\tMove down\n";
+    }
+    public function moveLeft(): void
+    {
+        echo $this->x -= $this->xSpeed;// TODO: Implement moveDown() method.
+        echo "\tMove left\n";
+    }
+    public function moveRight(): void
+    {
+        echo $this->x += $this->xSpeed;// TODO: Implement moveRight() method.
+        echo "\tMove right\n";
+    }
+}
+
+$point = new MovablePoint(1,3,5,8);
+echo $point . "\n\n";
+
+$point->moveUp();
+$point->moveRight();
+$point->moveDown();
+$point->moveLeft();
 ?>
+
